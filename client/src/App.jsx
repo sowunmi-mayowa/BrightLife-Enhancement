eae0a341812ec50d91b6c8f1d644d01a13f39116
sowunmi mayowa <smayowa176@gmail.com>
2024-10-02 22:49:37 +0100

    added index css file for tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
 import { Errorpage } from "./pages/Errorpage";
+import "./index.css"
 
 function App() {
   const router = createBrowserRouter(

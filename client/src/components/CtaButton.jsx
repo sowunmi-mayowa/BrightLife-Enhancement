8d433a9837b976e7e8d5462684e3e5012596b14f
sowunmi mayowa <smayowa176@gmail.com>
2024-10-05 00:16:26 +0100

    created the button for CTA

---
+++ client/src/components/CtaButton.jsx
@@ -0,0 +1,22 @@
+import * as Dialog from "@radix-ui/react-dialog"
+import Appointments from "./Appointments"
+
+const CtaButton = () => {
+  return (
+    <div>
+        <Dialog.Root>
+            <Dialog.Trigger className="bg-primaryGreen text-white px-5 py-2 rounded-md text-xs md:text-sm">
+               Book Appointment
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <Dialog.Overlay className="fixed inset-0 bg-black/50"  />
+                <Dialog.Content className="fixed bg-white p-4 rounded shadow-lg top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                    <Appointments />
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    </div>
+  )
+}
+
+export default CtaButton

73a05f6023a397e2c650a5944ef3b940b4db267b
sowunmi mayowa <49341447+sowunmi-mayowa@users.noreply.github.com>
2024-10-06 05:02:08 +0100

    Admin login feature (#2)
    * added middleware to check token
    
    * created admin controllers
    
    * created admin model for mongodb
    
    * installed new packages
    
    * installed new packages
    
    * updated frontend for login feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import Contact from "./pages/Contact";
 import Services from "./pages/Services";
 import { Errorpage } from "./pages/Errorpage";
 import "./index.css"
+import Login from "./pages/admin/Login";
+import Dashboard from "./pages/admin/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const router = createBrowserRouter(
@@ -16,6 +19,8 @@ function App() {
         <Route path="/about" element={ <About /> } />
         <Route path="/contact-us" element={ <Contact /> } />
         <Route path="/services" element={ <Services /> } />
+        <Route path="/admin/login" element={ <Login /> } />
+        <Route path="/admin/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
       </Route>
     )
   )
